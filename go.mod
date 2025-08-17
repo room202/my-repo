ae163f7aa36284b07e6c17042704a7511af5d543
room202 <yagi@i-seifu.jp>
2025-08-17 15:21:07 +0900

    Go言語追加

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/room202/my-repo
+
+go 1.25.0
---
+++ go/excellent/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestEvenOrOdd(t *testing.T) {
+	result := EvenOrOdd(10)
+
+	if result != "even" {
+		t.Errorf("expercted: even, actual: %s", result)
+	}
+}
